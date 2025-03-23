c39df5ec93a674c4fed1c6748b0a254a458c2ab7
anoop387 <163315029+anoop387@users.noreply.github.com>
2025-03-23 11:05:37 +0530

    Update main.py

--- code/src/DataProfiling/main.py
+++ code/src/DataProfiling/main.py
@@ -9,7 +9,6 @@ from streamlit_pandas_profiling import st_profile_report
 from ydata_profiling import ProfileReport
 from transformers import pipeline
 
-
 print("Hello, PyCharm!")
 st.title("Data Profiling App")
 st.subheader("This app will help you to do Data Exploration")
@@ -17,20 +16,30 @@ st.sidebar.header("User Input Features")
 uploaded_file = st. sidebar.file_uploader("upload your input Excel file", type=["csv"])
 if uploaded_file is not None:
     st .markdown("----")
-    input_df = pd.read_csv(uploaded_file, index_col=None)
-    profile = ProfileReport(input_df, title="Summary of the Data")
+    my_data = pd.read_csv(uploaded_file, index_col=None)
+
     st.subheader("Uploaded CSV File- Sample Dataset")
-    st.write(input_df)
-    st.subheader("Pandas Review")
-    st_profile_report(profile)
+    st.write(my_data)
 
     st.subheader("OpenAI Analysis")
-    os.environ['_BARD_API_KEY'] = "Your Key"
-    input_text = st.text_area("Regulatory Reporting Instruction")
-    my_data = input_df
-    answer = Bard().get_answer(f'{input_text} this data {my_data}')['content']
+    api_key = os.environ.get("_BARD_API_KEY")
+
+    default_multiline_text = """Transaction Amount should always match Reported Amount, except when the transaction involves cross-currency conversions, in which case a permissible deviation of up to 1% is allowed.
+Account Balance should never be negative, except in cases of overdraft accounts explicitly marked with an "OD" flag.
+Currency should be a valid ISO 4217 currency code, and the transaction must adhere to cross-border transaction limits as per regulatory guidelines.
+Country should be an accepted jurisdiction based on bank regulations, and cross-border transactions should include mandatory transaction remarks if the amount exceeds $10,000.Display Row Level Transaction Details"""
+
+    input_text = st.text_area("Regulatory Reporting Instruction",  height=250,value=default_multiline_text)
+    answer = Bard(token=api_key).get_answer(f'{input_text} this data {my_data}')['content']
     st.write(f'{answer}')
 
+    input_text2 = st.text_area("ChatBot - Raise Your Question")
+    answer2 = Bard(token=api_key).get_answer(f'{input_text2} this data {my_data}')['content']
+    st.write(f'{answer2}')
+
+    st.subheader("Pandas Review")
+    profile = ProfileReport(my_data, title="Summary of the Data")
+    st_profile_report(profile)
 
 else:
     st. markdown('----')
